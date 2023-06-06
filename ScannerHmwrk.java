ca3e10dcded154c0faf43b92cc76a2a8862693e5
Stirbumaxim <133904888+Stirbumaxim@users.noreply.github.com>
2023-06-06 21:39:43 +0300

    Add files via upload

---
+++ ScannerHmwrk.java
@@ -0,0 +1,26 @@
+package scanerHomework;
+
+import java.util.Scanner;
+
+public class ScannerHmwrk {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        System.out.println("Enter pls value for A");
+        int a = sc.nextInt();
+
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toOctalString(a));
+        System.out.println(Integer.toHexString(a));
+
+
+
+
+
+    }
+}
