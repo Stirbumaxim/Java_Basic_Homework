98ef6819d0c6c0c5af1774220e119dcb5dcc6bf5
Stirbumaxim <133904888+Stirbumaxim@users.noreply.github.com>
2023-06-08 05:19:00 +0300

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,22 @@
+package homework;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        double q = Math.pow(Math.E ,5 );
+        float w = (float) q;
+        double u = 1.5;
+        System.out.println(q + u);
+
+        double r = Math.log(2);
+        byte t = (byte) r;
+        short y = 14;
+        System.out.println(t + y);
+
+        int i = 1;
+        double o = 1.5;
+        System.out.println(i + o);
+
+
+    }
+}
