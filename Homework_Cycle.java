71558b95e000e77ac79d143219460dd05560a830
Stirbumaxim <133904888+Stirbumaxim@users.noreply.github.com>
2023-07-03 04:47:57 +0200

    Add files via upload

---
+++ Homework_Cycle.java
@@ -0,0 +1,12 @@
+public class Homework_Cycle {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++ ) {
+            int a = (int) Math.pow(2 , i);
+            System.out.print( a + " ");
+           
+        }
+
+
+
+    }
+}
